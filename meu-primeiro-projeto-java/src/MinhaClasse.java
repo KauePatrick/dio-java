7c2e096bb5ff59f1b2f0110b571fe9ca9abb023d
kaue_ <kauepatrick125@gmail.com>
2024-06-21 23:14:31 -0300

    Anatomia das classes 2

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -3,6 +3,17 @@ public class MinhaClasse {
 public static void main(String [] args){
 
     System.out.println("Eitann...");
+
+    final String BR = "kaue";
+
+    BR = "oii";
+
+    String br = "aiin";
+
+    br = "oii";
+
+    System.out.print(br);
+    System.out.print(BR);
 }
 
 } 
