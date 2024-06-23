25129e8778691abdc4655ad3f1fed471deae6e65
kaue_ <kauepatrick125@gmail.com>
2024-06-23 13:24:24 -0300

    Anatomia das Classes 4 - Identação

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -1,15 +1,18 @@
 public class MinhaClasse {
 
 public static void main(String [] args){
-    String primeiroNome = "Gleyson";
-    String segundoNome = "Sampaio";
+    
+        String primeiroNome = "Gleyson";
+        String segundoNome = "Sampaio";
 
-    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-    System.out.print(nomeCompleto);
-}
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.print(nomeCompleto);
+    }
 
  public static String nomeCompleto (String primeiroNome, String segundoNome){
+
     return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 
- }
-} 
+  }
+
+}  
