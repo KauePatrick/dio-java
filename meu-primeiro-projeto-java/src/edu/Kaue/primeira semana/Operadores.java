0f1c4eb1cfe1a657027d755baac22f47224da2f1
kaue_ <kauepatrick125@gmail.com>
2024-06-24 18:27:50 -0300

    Operadores 6

--- meu-primeiro-projeto-java/src/edu/Kaue/primeira semana/Operadores.java
+++ meu-primeiro-projeto-java/src/edu/Kaue/primeira semana/Operadores.java
@@ -22,5 +22,42 @@ public class Operadores {
 
         //Unarios
 
+        //Operador logico de negação (!)
+
+        //Operadores ternarios (?) (:)
+
+        int a, b;
+
+        a=5;
+        b=6;
+
+        String resultado = a==b ? "Verdadeiros" : "Falso";
+
+        System.out.println(resultado);
+
+
+        //Operadores Relacionais
+
+        int numero1, numero2;
+
+        numero1 = 1;
+        numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2){
+            System.out.println("A nossa condição e: " + simNao);
+        }
+
+        System.out.println("numeroUm e igual a numeroDois?" + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm e diferente a numeroDois?" + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm e maior a numeroDois?" + simNao);
+
+   
+
     }
 }
