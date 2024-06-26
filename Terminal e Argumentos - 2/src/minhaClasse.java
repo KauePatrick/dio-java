7c508a2fd5045d30abdb225cbdfb41d07b261da3
kaue_ <kauepatrick125@gmail.com>
2024-06-26 16:49:17 -0300

    Terminal e Argumentos - 2

---
+++ Terminal e Argumentos - 2/src/minhaClasse.java
@@ -0,0 +1,8 @@
+public class minhaClasse {
+
+
+    //ultilizando o comando java nomeDaClasse dentro do diretorio do arquivo .class pelo cmd e possivel executar o programa fora da ide.
+    public static void main(String[] args) {
+        System.out.println("Oi , fui executado pelo terminal");
+    }
+}
